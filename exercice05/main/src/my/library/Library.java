7ed5bfe9e7f77987edba37ca0dbe79868a23b1e1
Romain-gdev <78152569+Romain-gdev@users.noreply.github.com>
2022-12-02 17:03:25 +0100

    Update Library.java

--- exercice05/main/src/my/library/Library.java
+++ exercice05/main/src/my/library/Library.java
@@ -16,6 +16,10 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+/*
+* ESTEVES Matheo
+* GAUTIER Romain
+*/
 
 /**
  * <p>Classe Java pour anonymous complex type.
